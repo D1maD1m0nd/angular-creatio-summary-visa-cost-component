8e4e8faf199d81dccaa5fcaa4cae344b0cd575d3
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-02 12:28:24 +0300

    implementtion visa cost component

--- src/app/components/summary-visa-cost/vlt-summary-visa-cost.component.ts
+++ src/app/components/summary-visa-cost/vlt-summary-visa-cost.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'vlt-summary-visa-cost-component',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vlt-summary-visa-cost-component.scss']
 })
 export class VltSummaryVisaCostComponent implements OnInit {
-  constructor() { }
+  constructor(public apiService: ApiService) {
+  }
+
+  SendSaveDataIntoServer() {
+    this.apiService.PostSaveDataBudgetDetail().subscribe((i) => {
+      console.log(i)
+      this.apiService.ClearSaveData()
+    })
+  }
+
   ngOnInit(): void {
+    const params = new URLSearchParams(window.location.search);
+    const yearId = params.get('YearId');
+    const brandId = params.get('BrandId');
+    //?YearId=42533c5f-b173-4386-a1d9-8e02e5b91d4d&BrandId=f4c9e1ef-167e-4aef-b2c1-56950486df79
+    this.apiService.GetVisaSummary(yearId, brandId).subscribe((i) => {
+      console.log(i);
+    })
   }
 }
