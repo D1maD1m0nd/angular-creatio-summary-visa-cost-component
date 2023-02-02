ed4344405ccf0fea548e05439156216ec706e661
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-02 11:24:07 +0300

    gen summary-visa-cost-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SummaryVisaCostComponentComponent } from './components/summary-visa-cost-component/summary-visa-cost-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryVisaCostComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/summary-visa-cost-component/summary-visa-cost-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-visa-cost-component',
+  templateUrl: './summary-visa-cost-component.component.html',
+  styleUrls: ['./summary-visa-cost-component.component.scss']
+})
+export class SummaryVisaCostComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
