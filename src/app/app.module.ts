b504d49675c8410960726035622486247c3739fa
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-02 11:27:20 +0300

    add material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SummaryVisaCostComponentComponent } from './components/summary-visa-cost-component/summary-visa-cost-component.component';
+import {MatTableModule} from "@angular/material/table";
+import {MatCardModule} from "@angular/material/card";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatMenuModule} from "@angular/material/menu";
+import {ɵEmptyOutletComponent} from "@angular/router";
+import {MatSortModule} from "@angular/material/sort";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [
@@ -10,7 +23,20 @@ import { SummaryVisaCostComponentComponent } from './components/summary-visa-cos
     SummaryVisaCostComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatTableModule,
+    DragDropModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
