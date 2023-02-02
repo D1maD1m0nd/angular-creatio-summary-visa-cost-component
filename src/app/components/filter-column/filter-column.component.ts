f9e32a8224e610c836ac0e109e6c672864dfce07
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2023-02-02 11:57:31 +0300

    add filter-column.component.html

---
+++ src/app/components/filter-column/filter-column.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { ICostColumn } from 'src/app/data/model/response/CostColumn';
+import {FilteringColumnService} from "../../services/filtering-column.service";
+
+@Component({
+  selector: 'vlt-filter-column',
+  templateUrl: './filter-column.component.html',
+  styleUrls: ['./filter-column.component.scss']
+})
+export class FilterColumnComponent implements OnInit {
+
+  @Input() column: ICostColumn
+
+  constructor(public filterColumnService: FilteringColumnService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
